64349edc8b37c8c62d7c885533833a931faa3c9c
Aless <titifavotto@gmail.com>
2024-10-07 18:33:25 +0000

    nuevofaltamodificar

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  const palabras = {
+    articulo: ["El"],
+    adjetivo: ["pequeño", "joven", "jugueton", "solitario"],
+    sustantivo: ["perro"],
+    verbo: ["agarro", "mordio", "comio"],
+    objeto: ["casa", "cama", "mesa"],
+    lugar: ["en mi cama", "en mi escritorio"]
+  };
+  const level = {
+    facil: ["agarro"],
+    medio: ["mordio"],
+    dificil: ["comio"]
+  };
+  const colores = {
+    facil: ["green"],
+    medio: ["blue"],
+    dificil: ["red"]
+  };
+  const generarFrase = () => {
+    const frase = Object.values(palabras)
+      .map(arr => arr[Math.floor(Math.random() * arr.length)])
+      .join(" ");
+    const elementoFrase = document.getElementById("excuse");
+    elementoFrase.textContent = frase;
+    const level = document.getElementById("dificultad").value;
+    if (level === "facil") {
+      elementoFrase.style.color = colores["facil"];
+    } else if (level === "medio") {
+      elementoFrase.style.color = colores["medio"];
+    } else if (level === "dificil") {
+      elementoFrase.style.color = colores["dificil"];
+    }
+  };
+  const boton = document.createElement("button");
+  boton.textContent = "Generar nueva frase";
+  boton.addEventListener("click", generarFrase);
+  document.body.appendChild(boton);
 };
